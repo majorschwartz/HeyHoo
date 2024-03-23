d7d976103cd95437a38e1d84af01b43d0894dd12
Kieran Llarena <baymax709@gmail.com>
2024-03-23 16:33:15 -0400

    rm: frontend

--- frontend/components/RedButton.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-export default function RedButton({ bubbleState, setBubbleState }) {
-    return (
-        <View>
-            <Button onPress={() => bubbleState(true)} title="Red" color="red" />
-        </View>
-    );
-}
